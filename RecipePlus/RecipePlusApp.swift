755e8ab6cabde6442da04a7a0f698fa4c772329f
jack <jacklrogers06@gmail.com>
2025-03-01 16:51:01 -0500

    Initial Commit

---
+++ RecipePlus/RecipePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipePlusApp.swift
+//  RecipePlus
+//
+//  Created by Jack Rogers on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
